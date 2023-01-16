f10cca686836c48ed8e6010524a007801c53b345
David Jacimovic <david.jacimovic22@gmail.com>
2023-01-16 09:52:54 +0100

    Products fetched from API and actions taken care off with useReducer hook

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,31 +1,57 @@
-import React, { useContext, useReducer } from 'react'
-import reducer from '../reducers/products_reducer'
-import { SIDEBAR_CLOSE, SIDEBAR_OPEN } from '../actions'
+import React, { useContext, useEffect, useReducer } from "react";
+import reducer from "../reducers/products_reducer";
+import {
+  SIDEBAR_CLOSE,
+  SIDEBAR_OPEN,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+} from "../actions";
+import axios from "axios";
+import { products_url } from "../utils/constants";
 
 const initialState = {
   isSidebarOpen: false,
-}
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
+};
 
-const ProductsContext = React.createContext()
+const ProductsContext = React.createContext();
 
 export const ProductsProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const openSidebar = () => {
-    dispatch({ type: SIDEBAR_OPEN })
-  }
+    dispatch({ type: SIDEBAR_OPEN });
+  };
 
   const closeSidebar = () => {
-    dispatch({ type: SIDEBAR_CLOSE })
-  }
+    dispatch({ type: SIDEBAR_CLOSE });
+  };
+
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {}
+    dispatch({ type: GET_PRODUCTS_ERROR });
+  };
+
+  useEffect(() => {
+    fetchProducts(products_url);
+  }, []);
 
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
     </ProductsContext.Provider>
-  )
-}
+  );
+};
 
 export const useProductsContext = () => {
-  return useContext(ProductsContext)
-}
+  return useContext(ProductsContext);
+};
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,17 +1,41 @@
-import React from 'react'
-import { SIDEBAR_CLOSE, SIDEBAR_OPEN } from '../actions'
+import {
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_ERROR,
+  GET_PRODUCTS_SUCCESS,
+  SIDEBAR_CLOSE,
+  SIDEBAR_OPEN,
+} from "../actions";
 
 const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_OPEN) {
-    return { ...state, isSidebarOpen: true }
+    return { ...state, isSidebarOpen: true };
   }
 
   if (action.type === SIDEBAR_CLOSE) {
-    return { ...state, isSidebarOpen: false }
+    return { ...state, isSidebarOpen: false };
   }
 
-  return state
-  throw new Error(`No matching "${action.type}" - action type`)
-}
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
 
-export default products_reducer
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products,
+    };
+  }
+
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
+
+  throw new Error(`No matching "${action.type}" - action type`);
+};
+
+export default products_reducer;
