3568dfeb4ebc19249a16263dc88e49e0ebdea4d7
David Jacimovic <david.jacimovic22@gmail.com>
2023-01-18 11:48:05 +0100

    Images of the product displayed and logic for changing the main image. All of the products have same "secondary" images just to save some time.

--- src/components/ProductImages/ProductImages.js
+++ src/components/ProductImages/ProductImages.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./ProductImages.module.css";
 
-const ProductImages = () => {
-  return <div>product images</div>
-}
+const ProductImages = ({ images = [{ url: "" }] }) => {
+  const [main, setMain] = useState(images[0]);
+  return (
+    <section className={styles.product_images_container}>
+      <img src={main.url} alt="picture 1" className={styles.main_img} />
+      <div className={styles.product_images_gallery}>
+        {images.map((image, index) => {
+          return (
+            <img
+              src={image.url}
+              alt={image.filename}
+              key={index}
+              onClick={() => setMain(images[index])}
+              className={`${image.url === main.url && styles.active_img}`}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default ProductImages
+export default ProductImages;
